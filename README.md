e219dc887603bc0aed3cc6c32eb57d84c112adc7
Julien Stamm <5460657+julstamm@users.noreply.github.com>
2024-02-24 09:36:10 +0000

    Add the main program

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# rock-paper-scissors
-Console version of Rock Paper Scissors written in C
+# Rock Paper Scissors
+
+This is a console version of Rock Paper Scissors written in C.
+
+## Installation
+
+This game was developed on a Linux machine using VSCode and GCC 11. It can only be build through the VSCode task currently. The command used is:
+
+```bash
+gcc main.c -o main
+```
+
+## Usage
+
+Initial output:
+
+```bash
+Welcome to the Rock Paper Scissor Game!
+=======================================
+
+Type (1) for Rock, (2) for Papper, (3) for Scissor, (0) to quit
+Your choice:  
+```
+
+When user wins:
+
+```bash
+Type (1) for Rock, (2) for Papper, (3) for Scissor, (0) to quit
+Your choice: 1
+The computer's choice: 3
+
+--> You won
+```
+
+When user loses:
+
+```bash
+Type (1) for Rock, (2) for Papper, (3) for Scissor, (0) to quit
+Your choice: 1
+The computer's choice: 2
+
+--> You've lost
+```
+
+When there is a tie:
+
+```bash
+Type (1) for Rock, (2) for Papper, (3) for Scissor, (0) to quit
+Your choice: 1
+The computer's choice: 1
+
+--> No one wins
+```
+
+## What can be improved
+
+- Moving the VSCode task to a Makefile using something like CMake so it can be build on multiple environments
+- Remember the score between each rounds
+- Limit the number of rounds
+- Keep a score table in external file
---
+++ main.c
@@ -0,0 +1,67 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+enum Action {
+    Quit = 0,
+    Rock = 1,
+    Paper = 2,
+    Scissor = 3
+};
+
+bool user_wins(enum Action user_choice, enum Action computer_wins) {
+    return (user_choice == Rock && computer_wins == Scissor) |
+        (user_choice == Paper && computer_wins == Rock) |
+        (user_choice == Scissor && computer_wins == Paper);
+}
+
+int main(int argc, char const *argv[]) {
+    printf("Welcome to the Rock Paper Scissor Game!\n=======================================\n");
+
+    srand(time(0));
+
+    do {
+        enum Action user_choice = -1;
+        bool quit_game = false;
+
+        printf("\nType (1) for Rock, (2) for Papper, (3) for Scissor, (0) to quit\n");
+
+        do {
+            int int_user_choice = -1;
+            printf("Your choice: ");
+            scanf("%d", &int_user_choice);
+
+            if (int_user_choice == Quit) {
+                quit_game = true;
+            } else if (-1 <= int_user_choice & int_user_choice <= 3) {
+                user_choice = int_user_choice;
+            } else {
+                printf("Invalid input, try again\n");
+            }
+        } while ((user_choice == -1) & (quit_game == false));
+
+        if (quit_game) {
+            break;
+        }
+
+        enum Action computer_choice = (rand() % 3) + 1;
+
+        printf("The computer's choice: %d", computer_choice);
+        printf("\n\n--> ");
+
+        if (user_choice == computer_choice) {
+            printf("No one wins");
+        } else if (user_wins(user_choice, computer_choice)) {
+            printf("You won");
+        } else {
+            printf("You've lost");
+        }
+        printf("\n\n---------------------------------------\n");
+    } while (true);
+
+    printf("\nBye!\n\n");
+
+    return 0;
+}
